ab7a458f28d1dab276aace747e658c1bfe98de9e
emilio <emilio@mpbwalt.local>
2021-09-27 16:43:01 +0200

    reverted jedis pool

--- src/Server/SimpleTask.java
+++ src/Server/SimpleTask.java
@@ -239,12 +239,11 @@ public class SimpleTask {
 	public void initJedisPool(int poolSize, String poolAddr) {
 		JedisPoolConfig cfg = new JedisPoolConfig();
 		cfg.setMaxTotal(poolSize);
-		cfg.setMaxWaitMillis(60000);
 		this.jedisPool = new JedisPool(cfg, poolAddr);
 	}
 
 	public void initJedisPool() {
-		this.initJedisPool(200, this.jedisHost);
+		this.initJedisPool(2000, this.jedisHost);
 	}
 
 	public void initThreadPoolExecutor() {
