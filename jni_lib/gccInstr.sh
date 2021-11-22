fddc28fb728cff9d2d3e5bfd42b18eefa31e4e15
emilio <emilio@mpbwalt.local>
2021-11-22 14:13:06 +0100

    minor

--- jni_lib/gccInstr.sh
+++ jni_lib/gccInstr.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 gcc -I${JAVA_HOME}/include -I${JAVA_HOME}/include/linux -fPIC -shared jni_GetThreadID.c -o libGetThreadID.so
-LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+LD_LIBRARY_PATH=$(pwd)
 export LD_LIBRARY_PATH
