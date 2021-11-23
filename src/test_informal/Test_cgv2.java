465389fed6fa6f419dfc3f1f4bade53ea4aec017
emilio <emilio@mpbwalt.local>
2021-11-23 15:44:56 +0100

    test cgv2

--- src/test_informal/Test_cgv2.java
+++ src/test_informal/Test_cgv2.java
@@ -5,64 +5,59 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
+
 import jni.GetThreadID;
 
 public class Test_cgv2 {
 
-	private class Sched implements Runnable {
-
-		@Override
-		public void run() {
-			while (true) {
-				try {
-					TimeUnit.MILLISECONDS.sleep(100);
-					System.out.println("Sched alive");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-	}
-
-	private class Srv implements Runnable {
-
-		@Override
-		public void run() {
-			while (true) {
-				try {
-					TimeUnit.MILLISECONDS.sleep(100);
-					System.out.println("Srv alive");
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-	}
 
 	public static void main(String[] args) {
 		int tid = GetThreadID.get_tid();
-		
+
+		System.out.println("PID del main process:" + tid);
+
 		// aggiungo questo processo al gruppo principale del tier
 		BufferedWriter out;
 		try {
-			out = new BufferedWriter(new FileWriter("/sys/fs/cgroup/tier1/cgroup.procs",true));
+			out = new BufferedWriter(new FileWriter("/sys/fs/cgroup/tier1/cgroup.procs", true));
 			out.write(String.valueOf(tid));
 			out.flush();
-	        out.close();
+			out.close();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        
 
 		// creo il threadpool di serventi
 		ExecutorService sched = java.util.concurrent.Executors.newFixedThreadPool(1);
+		sched.submit(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						TimeUnit.MILLISECONDS.sleep(100);
+						System.out.println("Sched alive");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 		// creo il threadpool dello scheduler
 		ExecutorService serv = java.util.concurrent.Executors.newFixedThreadPool(1);
-
-
+		serv.submit(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						TimeUnit.MILLISECONDS.sleep(100);
+						System.out.println("Srv alive");
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 		// ogni servente si aggiunge al gruppo srv
 		// ogni scheduler si aggiunge al gruppo sched
 		try {
