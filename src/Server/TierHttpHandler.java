a82178cbe473c06cce564c2f2ef00ccf91057238
emilio <emilio@mpbwalt.local>
2021-11-30 08:31:08 +0100

    last attempt

--- src/Server/TierHttpHandler.java
+++ src/Server/TierHttpHandler.java
@@ -56,17 +56,17 @@ public abstract class TierHttpHandler implements Runnable {
 
 	public abstract String getName();
 
-	public int doWorkCPU() {
-		// long delay = Long.valueOf(Math.round(dist.sample() * 1000000));
-//		long start = this.mgm.getCurrentThreadCpuTime();
-//		while ((this.mgm.getCurrentThreadCpuTime() - start) < delay) {
-//		}
-		long delay = 10000000;
-		int k = 0;
-		while (k < delay) {
-			k++;
+	public void doWorkCPU() {
+		long delay = Long.valueOf(Math.round(dist.sample() * 1000000));
+		long start = this.mgm.getCurrentThreadCpuTime();
+		while ((this.mgm.getCurrentThreadCpuTime() - start) < delay) {
 		}
-		return k;
+//		long delay = 10000000;
+//		int k = 0;
+//		while (k < delay) {
+//			k++;
+//		}
+//		return 0;
 	}
 
 	public void doWorkSleep(float executing) throws InterruptedException {
