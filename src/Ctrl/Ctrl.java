2f804f8c6028733a4f554868619a9bc7543e1b06
emilio-imt <emilio-imt@MacBook-Pro.local>
2022-11-03 22:48:44 +0100

    minor

--- src/Ctrl/Ctrl.java
+++ src/Ctrl/Ctrl.java
@@ -177,7 +177,7 @@ public class Ctrl extends Thread {
 			}
 			
 			try {
-				TimeUnit.MILLISECONDS.sleep(10);
+				TimeUnit.MILLISECONDS.sleep(30);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
